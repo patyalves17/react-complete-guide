d923932fc89931a4cf377b893854157cbf45fd23
patricia_aalmeida <patricia.almeida@fncit.com.br>
2018-11-27 09:03:03 -0200

    working with state, handling events, two way binding, styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
+
+
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Paty', age: 29 },
+      { name: 'Fulano', age: 30 },
+      { name: 'Ciclano', age: 21 }
+    ]
+  }
+
+  swhitchNameHandler = (newName) => {
+    console.log("change name");
+    this.setState({
+      persons: [
+        { name: newName, age: 29 },
+        { name: 'Fulano', age: 30 },
+        { name: 'Ciclano', age: 21 }
+      ]
+    });
+  }
+
+  changeNameHandler = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      persons: [
+        { name: 'Paty', age: 29 },
+        { name: event.target.value, age: 30 },
+        { name: 'Ciclano', age: 21 }
+      ]
+    });
+  }
+
   render() {
+
+    const style = {
+      background: 'red'
+    }
+
     return (
       <div className="App">
-       <p>teste</p>
-       <Person name="Paty" age="29" > Hobbie: watch Tv series</Person>
+        <p>teste</p>
+        <button
+          onClick={this.swhitchNameHandler.bind(this, "Patricia")}
+          style={style} >
+          Switch Name</button>
+        <Person
+          click={this.swhitchNameHandler.bind(this, "Patricia !!!")}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}  >
+          Hobbie: watch Tv series
+       </Person>
+        <Person click={() => this.swhitchNameHandler("Paty!!!! ")}
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.changeNameHandler} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
+
       </div>
     );
   }
