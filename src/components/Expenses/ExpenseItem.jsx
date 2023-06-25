36693d4c31ab3c780fb78d8edf5dd7ec31d24382
patyalves17 <patriciaalves10@gmail.com>
2023-06-25 17:27:59 -0300

    [feat] setState

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import Card from './../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [titleEdit, setTitle] = useState(title);
+
+  const onClickHandler = () => {
+    setTitle('Change!!!');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{titleEdit}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
+      <button onClick={onClickHandler}>Chance Title</button>
     </Card>
   );
 };
