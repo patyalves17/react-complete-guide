a48c61f219909b9d3b43f0a7bb0ee5f9386d5990
patyalves17 <patriciaalves10@gmail.com>
2023-06-25 23:28:55 -0300

    [feat] add new expense

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
+const INITIAL_EXPENSES = [
+  {
+    id: '1',
+    title: 'Car Insurance',
+    amount: 296.98,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: '2',
+    title: 'New Desk',
+    amount: 450.0,
+    date: new Date(2023, 2, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: '1',
-      title: 'Car Insurance',
-      amount: 296.98,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: '2',
-      title: 'New Desk',
-      amount: 450.0,
-      date: new Date(2023, 2, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+
+  const addExpenseHandler = (enteredExpenseData) => {
+    setExpenses((prevExpenses) => [enteredExpenseData, ...prevExpenses]);
+  };
+
   return (
     <>
-      <h1>React complete guide 2023</h1>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </>
   );
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import Card from './../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = ({ expenses }) => {
+  const [filtered, setFilterdYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilterdYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        onChangeFilter={filterChangeHandler}
+        selected={filtered}
+      />
       {expenses &&
         expenses.map((expense) => (
           <ExpenseItem
